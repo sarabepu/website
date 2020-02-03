1f95f8cfdc5feb76a1f5a22e61e466454c956f4a
Sara Bejarano <sm.bejarano44@uniandes.edu.co>
2020-02-03 11:06:10 -0500

    animated progressbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,32 @@
 (function ($) {
-    
-    
+
+
     /*--/ Navbar Menu Reduce /--*/
     $("body").scrollspy({ target: "#mainNav" });
     $(window).on("scroll", function () {
-        var section = $("#about").offset().top; 
-        
-        if ($(window).scrollTop() > section-30) {
+        var section = $("#about").offset().top;
+
+        if ($(window).scrollTop() > section - 30) {
             $(".navbar-expand-md").addClass("navbar-reduce");
             $(".navbar-expand-md").removeClass("navbar-trans");
         } else {
             $(".navbar-expand-md").addClass("navbar-trans");
             $(".navbar-expand-md").removeClass("navbar-reduce");
         }
-        
-    });
-    
-    /*--/ Star Typed /--*/
-    if ($(".text-slider").length == 1)
-    {
-        var typed_strings = "Web Developer,Programmer,Pianist,Hip-Hop Dancer,Salsa lover,Latina,Worshiper";
 
-        var options = {
-            strings:typed_strings.split(","),
-            typeSpeed: 80,
-            loop: true,
-            backDelay: 1100,
-            backSpeed: 30
-        };
-    }
+    });
+
+    /*--/ Star Typed /--*/
+
+    var typed_strings = "Web Developer,Programmer,Pianist,Hip-Hop Dancer,Salsa lover,Latina,Worshiper";
+
+    var options = {
+        strings: typed_strings.split(","),
+        typeSpeed: 80,
+        loop: true,
+        backSpeed: 30
+    };
+
     // eslint-disable-next-line no-undef
-    new Typed(".text-slider",options);
+    new Typed(".text-slider", options);
 })(jQuery);
